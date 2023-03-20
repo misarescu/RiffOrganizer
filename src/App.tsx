e769ad39afe558aa8f7346191d5af92b783a6637
redshadowfx <mihai.isarescu@zohomail.eu>
2023-03-20 20:26:33 +0200

    added a ui directory and some ui components files. no implementation yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,13 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
-import './index.css';
-
 function App() {
-  const [countUp, setCountUp] = useState(0);
-  const [countDown, setCountDown] = useState(0);
-
   return (
-    <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCountUp((count) => count + 10)}>
-          count is {countUp}
-        </button>
-        <button onClick={() => setCountDown((count) => count - 10)}>
-          count is {countDown}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+    <>
+      <h1 className='text-4xl font-extrabold dark:text-white'>
+        Welcome to Riff Organizer!
+      </h1>
+      <p className='text-3xl font-bold underline'>
+        The place where you will start your guitar journey!
       </p>
-    </div>
+    </>
   );
 }
 
