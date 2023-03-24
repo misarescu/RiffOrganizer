edbdb126f17cab309e784713ab26a125ee97e33b
redshadowfx <mihai.isarescu@zohomail.eu>
2023-03-24 13:25:01 +0200

    Added the image of the guitar to the homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import welcomeImgUrl from '../assets/WelcomePageImage.png';
 
 function HomePage() {
-  return <h1>HomePage</h1>;
+  return (
+    <>
+      <img src={welcomeImgUrl} className=' w-screen lg:h-1/4 lg:object-cover' />
+      <h1 className='text-4xl font-extrabold dark:text-white'>
+        Welcome to Riff Organizer!
+      </h1>
+      <p className='text-3xl font-bold underline'>
+        The place where you will start your guitar journey!
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
