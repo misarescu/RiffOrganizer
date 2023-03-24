54e8078447753e2dcb563743e5189815412991a0
redshadowfx <mihai.isarescu@zohomail.eu>
2023-03-24 13:05:03 +0200

    Search is now done

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import logoImgUrl from '../../assets/Logo.png';
 import Button from './Button';
+import Search from './Search';
 
 function NavBar() {
   return (
     <nav className='w-screen h-auto bg-blue-500 flex flex-row p-2 justify-between items-center'>
       <div className='h-auto w-auto flex flex-row items-center space-x-2 '>
         <img src={logoImgUrl} className='h-12 w-12' />
-        <h1 className='hidden lg:inline-flex text-2xl text-bold font-semibold font-serif'>
+        <h1 className='hidden lg:inline-flex text-2xl text-bold font-semibold font-serif text-slate-800'>
           Riff Organizer
         </h1>
       </div>
-      <input
-        className='w-1/2 text-2xl text-bold font-semibold font-serif bg-slate-50 rounded-full'
-        placeholder='Searchbar'
-      />
+      <Search isVisible={true} />
 
       <div className='w-auto h-max flex justify-center items-center flex-col space-y-2 space-x-0 lg:flex-row lg:space-y-0 lg:space-x-2 grow-0'>
         <Button special className='text-xs lg:text-base'>
