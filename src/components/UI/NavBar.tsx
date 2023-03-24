cd91c5593641eda65e1b376544d5f6f12133086f
redshadowfx <mihai.isarescu@zohomail.eu>
2023-03-25 01:40:28 +0200

    updated the logic for showing the searchbar, task it sourced now in navbar; also in navbar there is now a logged in logitc to render different states of the bar like username/icon, search bar and auth and log off buttons

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logoImgUrl from '../../assets/Logo.png';
+import defaultUserImgUrl from '../../assets/DefaultUserIcon.svg';
 import Button from './Button';
 import Search from './Search';
 
+const USER_NAME = 'Mihai Smecheru';
+
 function NavBar() {
+  // set locally for now for prototyping purposes
+  // TODO: set auth with the db
+  const [userLoggedIn, setUserLoggedIn] = useState(true);
+
   return (
-    <nav className='w-full h-auto bg-blue-500 flex flex-row p-2 justify-between items-center'>
-      <div className='h-auto w-auto flex flex-row items-center space-x-2 '>
-        <img src={logoImgUrl} className='h-12 w-12' />
+    <nav className='w-full h-auto bg-blue-500 flex flex-row p-2 space-x-2 justify-between items-center'>
+      <div className='h-auto w-auto flex flex-row items-center space-x-2 shrink-0'>
+        {!userLoggedIn ? (
+          <img src={logoImgUrl} className='h-12 w-12' />
+        ) : (
+          // placeholder image for now
+          // TODO:should be fetched from the db
+          <img
+            src={defaultUserImgUrl}
+            className='h-12 w-12 bg-slate-200 rounded-full p-1'
+          />
+        )}
+
         <h1 className='hidden md:inline-flex text-2xl text-bold font-semibold font-serif text-slate-800'>
-          Riff Organizer
+          {/* placeholder name for now 
+          TODO: should be fetched from the db */}
+          {userLoggedIn ? USER_NAME : 'Riff Organizer'}
         </h1>
       </div>
-      <Search isVisible={false} />
-
-      <div className='w-auto h-max flex justify-center items-center flex-col space-y-1 space-x-0 md:flex-row md:space-y-0 md:space-x-2 grow-0'>
-        <Button special>Sign up</Button>
-        <Button special>Sign in</Button>
-      </div>
+      {userLoggedIn ? <Search /> : null}
+      {!userLoggedIn ? (
+        <div className='w-auto h-max flex justify-center items-center flex-row space-y-0 space-x-2 grow-0 shrink-0'>
+          <Button
+            special
+            onClick={() => {
+              alert('make a new account');
+            }}>
+            Sign Up
+          </Button>
+          <Button special onClick={() => setUserLoggedIn(true)}>
+            Sign In
+          </Button>
+        </div>
+      ) : (
+        <Button
+          className='grow-0 shrink-0'
+          onClick={() => setUserLoggedIn(false)}>
+          Sign Out
+        </Button>
+      )}
     </nav>
   );
 }
--- src/components/UI/Search.tsx
+++ src/components/UI/Search.tsx
@@ -1,42 +1,36 @@
 import React from 'react';
 
-type SearchProps = {
-  isVisible?: boolean;
-};
-
-function Search(props: SearchProps) {
+function Search() {
   return (
     <>
-      {props.isVisible ? (
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-          className='w-1/2'>
-          <div className='relative '>
-            <div className='absolute inset-y-0 left-0 pl-2 pointer-events-none flex items-center'>
-              <svg
-                aria-hidden='true'
-                className='w-5 h-5 text-slate-800 dark:text-slate-200'
-                fill='none'
-                stroke='currentColor'
-                viewBox='0 0 24 24'
-                xmlns='http://www.w3.org/2000/svg'>
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'></path>
-              </svg>
-            </div>
-            <input
-              className='w-full block rounded-md pl-9 text-2xl text-bold font-semibold font-serif placeholder-slate-400 dark:placeholder-slate-600 text-slate-800 dark:text-slate-200 bg-slate-200 dark:bg-slate-800'
-              placeholder='Search'
-              type='text'
-            />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className='w-1/2'>
+        <div className='relative '>
+          <div className='absolute inset-y-0 left-0 pl-2 pointer-events-none flex items-center'>
+            <svg
+              aria-hidden='true'
+              className='w-5 h-5 text-slate-800 dark:text-slate-200'
+              fill='none'
+              stroke='currentColor'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'>
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'></path>
+            </svg>
           </div>
-        </form>
-      ) : null}
+          <input
+            className='w-full block rounded-md pl-9 text-2xl text-bold font-semibold font-serif placeholder-slate-400 dark:placeholder-slate-600 text-slate-800 dark:text-slate-200 bg-slate-200 dark:bg-slate-800'
+            placeholder='Search'
+            type='text'
+          />
+        </div>
+      </form>
     </>
   );
 }
