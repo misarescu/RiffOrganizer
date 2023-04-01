495005a7ef24398a00f98263fc9a81ab62992a92
redshadowfx <mihai.isarescu@zohomail.eu>
2023-04-02 00:53:33 +0300

    add the store and store provider in the App component to make it visible globally

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import RootLayout from './layouts/Root';
 import HomePage from './pages/Home';
 import UserPage from './pages/User';
+import { Provider } from 'react-redux';
+import store from './store/index';
 
 const router = createBrowserRouter([
   {
@@ -18,7 +20,11 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 }
 
 export default App;
