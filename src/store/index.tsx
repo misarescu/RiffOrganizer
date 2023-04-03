f199f262cfe60287fa34a4d55e78ae9eb26bb81a
redshadowfx <mihai.isarescu@zohomail.eu>
2023-04-03 18:18:29 +0300

    added user data to the store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { supabaseReducer } from './supabase-slice';
+import { userReducer } from './user-slice';
 
 const store = configureStore({
-  reducer: { API: supabaseReducer },
+  reducer: { API: supabaseReducer, user: userReducer },
 });
 
 export default store;
---
+++ src/store/user-slice.tsx
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isAuthenticated: false,
+  name: '',
+  email: '',
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const userActions = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
