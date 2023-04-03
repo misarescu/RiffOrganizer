b84cc41ff5480c07c9f92cca97578d53102dc78e
redshadowfx <mihai.isarescu@zohomail.eu>
2023-04-03 18:47:02 +0300

    updated the navbar to use the store states for user auth

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { userActions } from '../../store/user-slice';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { StoreStateType } from '../../store/index';
 import logoImgUrl from '../../assets/Logo.png';
 import defaultUserImgUrl from '../../assets/DefaultUserIcon.svg';
 import Button from './Button';
 import Search from './Search';
+import dbClient from '../../API/dbClient';
 
 // maybe a burger button could be better than the user profile
 // import burgerMenuImgUrl from '../../assets/BurgerMenu.svg';
@@ -11,25 +16,27 @@ import Search from './Search';
 const USER_NAME = 'Mihai Smecheru';
 
 function NavBar() {
-  // set locally for now for prototyping purposes
-  // TODO: set auth with the db
-  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  // get user authenticated status
+  const userIsAuthenticated = useSelector(
+    (state: StoreStateType) => state.user.isAuthenticated
+  );
+  const dispatch = useDispatch();
 
   const navigate = useNavigate();
 
   function logInHandler() {
-    setUserLoggedIn(true);
+    dispatch(userActions.login());
     navigate('/user');
   }
   function logOutHandler() {
-    setUserLoggedIn(false);
+    dispatch(userActions.logout());
     navigate('/');
   }
 
   return (
     <nav className='w-full h-auto bg-blue-500 flex flex-row p-2 space-x-2 justify-between items-center'>
       <div className='h-auto w-auto flex flex-row items-center space-x-2 shrink-0'>
-        {!userLoggedIn ? (
+        {!userIsAuthenticated ? (
           <img src={logoImgUrl} className='h-12 w-12' />
         ) : (
           // placeholder image for now
@@ -44,12 +51,12 @@ function NavBar() {
         <h1 className='hidden md:inline-flex text-2xl text-bold font-semibold font-serif text-slate-800'>
           {/* placeholder name for now 
           TODO: should be fetched from the db */}
-          {userLoggedIn ? USER_NAME : 'Riff Organizer'}
+          {userIsAuthenticated ? USER_NAME : 'Riff Organizer'}
         </h1>
       </div>
       {/* TODO: introduce a LogInForm component to handle the loging on and then navigate to the UserPage */}
-      {userLoggedIn ? <Search /> : null}
-      {!userLoggedIn ? (
+      {userIsAuthenticated ? <Search /> : null}
+      {!userIsAuthenticated ? (
         <div className='w-auto h-max flex justify-center items-center flex-row space-y-0 space-x-2 grow-0 shrink-0'>
           <Button
             special
