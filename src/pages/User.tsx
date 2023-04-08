fafc2618b92673dfb4263670eb4a567b9565455d
redshadowfx <mihai.isarescu@zohomail.eu>
2023-04-08 15:20:49 +0300

    updated the loader to include the song list when loading the user data

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -8,54 +8,65 @@ import { userActions } from '../store/user-slice';
 import AddSongForm from '../components/AddSongForm';
 import { songsActions } from '../store/songs-slice';
 
-type UserDetailType = {
+type UserData = {
   email: string;
   full_name: string;
+  id: string;
+  songs?: {
+    id: string;
+    artist_name: string;
+    song_name: string;
+    sections?: {
+      name: string;
+      status: string;
+    };
+  }[];
 };
 
-const listOfSongs: string[] = [
-  // 'Crazy Train – Ozzy Osbourne',
-  // 'Holy Wars… The Punishment Due – Megadeth',
-  // ' Iron Man – Black Sabbath',
-  // ' Master Of Puppets – Metallica ',
-  // 'Back In Black – AC/DC',
-  // 'Breaking The Law – Judas Priest',
-  // 'The Trooper – Iron Maiden',
-  // 'Ace Of Spades – Motörhead',
-  // 'Immigrant Song – Led Zeppelin',
-  // 'Shout At The Devil – Mötley Crüe',
-  // 'Welcome To My Nightmare – Alice Cooper',
-  // 'Goddamn Electric – Pantera',
-  // 'Sweet Child O’ Mine – Guns n’ Roses',
-  // 'Angel Of Death – Slayer',
-  // 'Belly Of The Beast – Anthrax',
-  // 'Ritual – Ghost',
-  // 'Holy Diver – Dio',
-  // ' Rock You Like A Hurricane – Scorpions',
-  // ' Eyes Of A Stranger – Queensrÿche',
-  // 'The Moor – Opeth',
-  // 'Black Label – Lamb of God',
-  // 'Metal Health (Bang Your Head) – Quiet Riot',
-  // 'Black No. 1 (Little Miss Scare-All) – Type-O Negative',
-  // 'One – Metallica',
-  // 'Stargazer – Rainbow',
-  // 'Memento Mori - Lamb of God',
-];
+// const listOfSongs: string[] = [
+// 'Crazy Train – Ozzy Osbourne',
+// 'Holy Wars… The Punishment Due – Megadeth',
+// ' Iron Man – Black Sabbath',
+// ' Master Of Puppets – Metallica ',
+// 'Back In Black – AC/DC',
+// 'Breaking The Law – Judas Priest',
+// 'The Trooper – Iron Maiden',
+// 'Ace Of Spades – Motörhead',
+// 'Immigrant Song – Led Zeppelin',
+// 'Shout At The Devil – Mötley Crüe',
+// 'Welcome To My Nightmare – Alice Cooper',
+// 'Goddamn Electric – Pantera',
+// 'Sweet Child O’ Mine – Guns n’ Roses',
+// 'Angel Of Death – Slayer',
+// 'Belly Of The Beast – Anthrax',
+// 'Ritual – Ghost',
+// 'Holy Diver – Dio',
+// ' Rock You Like A Hurricane – Scorpions',
+// ' Eyes Of A Stranger – Queensrÿche',
+// 'The Moor – Opeth',
+// 'Black Label – Lamb of God',
+// 'Metal Health (Bang Your Head) – Quiet Riot',
+// 'Black No. 1 (Little Miss Scare-All) – Type-O Negative',
+// 'One – Metallica',
+// 'Stargazer – Rainbow',
+// 'Memento Mori - Lamb of God',
+// ];
 
 function UserPage() {
-  const userDetails = useRouteLoaderData('user-page') as UserDetailType;
+  const userData = useRouteLoaderData('user-page') as UserData;
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(
       userActions.setUserInfo({
-        email: userDetails.email,
-        fullName: userDetails.full_name,
+        email: userData.email,
+        fullName: userData.full_name,
+        userId: userData.id,
       })
     );
     dispatch(userActions.login());
-    console.log(userDetails);
-  }, [userDetails]);
+    console.log(userData);
+  }, [userData]);
 
   function addSongHandler() {
     dispatch(songsActions.openSongForm());
@@ -69,14 +80,16 @@ function UserPage() {
           Add a New Song
         </Button>
 
-        {listOfSongs.length === 0 ? (
+        {userData.songs?.length === 0 ? (
           <Card>
             <p>Sorry you don't have any songs added yet</p>
           </Card>
         ) : (
-          listOfSongs.map((message) => (
-            <Card title={message.trim()}>
-              <p>{message.trim()}</p>
+          userData.songs?.map((song) => (
+            <Card
+              key={song.id}
+              title={`${song.song_name} - ${song.artist_name}`}>
+              <p>Your progress:</p>
               <Button>Add Section</Button>
             </Card>
           ))
@@ -88,7 +101,7 @@ function UserPage() {
 
 export default UserPage;
 
-async function loadUserDetails(urlUserId: string): Promise<Response> {
+async function loadUserData(urlUserId: string): Promise<Response> {
   const { data: sessionData } = await dbClient.auth.getSession();
   const currentSession = sessionData.session;
   if (!currentSession) {
@@ -97,7 +110,22 @@ async function loadUserDetails(urlUserId: string): Promise<Response> {
 
   const userQuery = await dbClient
     .from('user_info')
-    .select('*')
+    .select(
+      `
+      id,
+      full_name,
+      email,
+      songs(
+        id,
+        artist_name,
+        song_name,
+        sections(
+          name,
+          status
+        )
+      )
+    `
+    )
     .eq('id', urlUserId);
 
   if (userQuery.data?.length === 0) {
@@ -106,6 +134,7 @@ async function loadUserDetails(urlUserId: string): Promise<Response> {
 
   return json({ ...userQuery.data?.at(0) });
 }
+
 export async function loader({ params }: any): Promise<Response> {
-  return await loadUserDetails(params.userId);
+  return await loadUserData(params.userId);
 }
